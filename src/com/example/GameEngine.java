097daff9a48d5ebcefb55cdd846cd60f1545b2f2
shivanijain <jainshivani99@yahoo.com>
2018-02-19 03:27:38 -0600

    Modified receiveInitialHand() in Shivani.java and modified playFirstRound() in GameEngine.java

--- src/com/example/GameEngine.java
+++ src/com/example/GameEngine.java
@@ -1,168 +1,197 @@
 package com.example;
 
 import java.util.Collections;
-import java.util.Comparator;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameEngine {
+    //-------------------------------------------------------------------------------
+    //Class Properties
 
     private static List<Card> deck;
     private static List<Card> discardPile = new ArrayList<Card>();
-    private static Shivani p1 = new Shivani();
-    private static Shivani p2 = new Shivani();
-    private static int deadwoodPoints;
-    private static List<Card> cardsNotInMeld = p1.getHand();
+    private static PlayerStrategy p1 = new Shivani();
+    private static PlayerStrategy p2 = new Jack();
+    private static PlayerStrategy p3 = new Jill();
+    //-------------------------------------------------------------------------------
+    // Execute Main Method
 
     public static void main(String[] args) {
+
+        //-------------------------------------------------------------------------------
+        // Initialize deck, shuffle deck, deal deck, create discard pile, play first round and play normalRounds
+
         GameEngine.initializeDeck();
         //GameEngine.shuffleDeck();
         GameEngine.dealDeck();
 
-        //Adding a card to the discard pile
-        discardPile.add(deck.get(0));
-        deck.remove(0);
-
-        //GameEngine.playFirstRound();
-        //List<Card> leftoverCardsFromRunMeld = new ArrayList<Card>();
-        List<Meld> totalMelds = new ArrayList<Meld>();
-
-        //Sort run meld based on suit and then rank
-        List<Card> p1PotentialRunMeld = new ArrayList<>(cardsNotInMeld);
-        Collections.sort(p1PotentialRunMeld, new Comparator<Card>() {
-            @Override
-            public int compare(Card o1, Card o2) {
-                int suitDifference = o1.getSuit().ordinal() - o2.getSuit().ordinal();
-                if (suitDifference == 0) {
-                    return o1.getRankValue() - o2.getRankValue();
-                }
-                return suitDifference;
-            }
-        });
-        //List<Card> p1PotentialRunMeld2 = new ArrayList<Card>();
-        //p1PotentialRunMeld2.add(p1PotentialRunMeld.get(0));
-        //p1PotentialRunMeld2.add(p1PotentialRunMeld.get(1));
-        //RunMeld p1RunMeld = Meld.buildRunMeld(p1PotentialRunMeld);
-
-        //Sort set meld based on rank
-        List<Card> p1PotentialSetMeld = new ArrayList(cardsNotInMeld);
-        Collections.sort(p1PotentialSetMeld, new Comparator<Card>() {
-            @Override
-            public int compare(Card o1, Card o2) {
-                return o1.getRankValue() - o2.getRankValue();
-            }
-        });
-        //iterator i - if the 4th element is the same, you can assume that the the cards in the middle are the same because
-        //they are sorted
-        int i = 0;
-        while (i < p1PotentialSetMeld.size()) {
-            boolean canCheck4 = (i + 3) < p1PotentialSetMeld.size();
-            boolean canCheck3 = (i + 2) < p1PotentialSetMeld.size();
-            if (canCheck4 && p1PotentialSetMeld.get(i).getRank().equals(p1PotentialSetMeld.get(i + 3).getRank())) {
-                List<Card> p1PotentialSetMeld2 = new ArrayList<Card>();
-                p1PotentialSetMeld2.add(p1PotentialSetMeld.get(i));
-                p1PotentialSetMeld2.add(p1PotentialSetMeld.get(i + 1));
-                p1PotentialSetMeld2.add(p1PotentialSetMeld.get(i + 2));
-                p1PotentialSetMeld2.add(p1PotentialSetMeld.get(i + 3));
-                cardsNotInMeld.remove(p1PotentialSetMeld.get(i));
-                cardsNotInMeld.remove(p1PotentialSetMeld.get(i + 1));
-                cardsNotInMeld.remove(p1PotentialSetMeld.get(i + 2));
-                cardsNotInMeld.remove(p1PotentialSetMeld.get(i + 3));
-                SetMeld p1SetMeld = Meld.buildSetMeld(p1PotentialSetMeld2);
-                totalMelds.add(p1SetMeld);
-                i += 4;
-            } else if (canCheck3 && p1PotentialSetMeld.get(i).getRank().equals(p1PotentialSetMeld.get(i + 2).getRank())) {
-                List<Card> p1PotentialSetMeld3 = new ArrayList<Card>();
-                p1PotentialSetMeld3.add(p1PotentialSetMeld.get(i));
-                p1PotentialSetMeld3.add(p1PotentialSetMeld.get(i + 1));
-                p1PotentialSetMeld3.add(p1PotentialSetMeld.get(i + 2));
-                cardsNotInMeld.remove(p1PotentialSetMeld.get(i));
-                cardsNotInMeld.remove(p1PotentialSetMeld.get(i + 1));
-                cardsNotInMeld.remove(p1PotentialSetMeld.get(i + 2));
-                SetMeld p1SetMeld2 = Meld.buildSetMeld(p1PotentialSetMeld3);
-                totalMelds.add(p1SetMeld2);
-                i += 3;
-            } else {
-                i++;
-            }
-        }
-        System.out.println("");
+        GameEngine.playFirstRound();
+        //GameEngine.playNormalRound();
     }
 
-    //Retrieving all cards in the game and setting up the deck
+    //-------------------------------------------------------------------------------
+    //Initialize the deck: Retrieving all cards in the game and setting up the deck
+
     public static void initializeDeck() {
         deck = new ArrayList<Card>();
         deck.addAll(Card.getAllCards());
         Collections.sort(deck);
     }
 
+    //-------------------------------------------------------------------------------
     //Shuffling the deck
+
     public static void shuffleDeck() {
         Collections.shuffle(deck);
     }
 
-    //Dealing ten cards to each player
+    //-------------------------------------------------------------------------------
+    //Dealing the deck: Dealing ten cards to each player
+
     public static void dealDeck() {
+        List<Card> hand1 = new ArrayList<Card>();
+        List<Card> hand2 = new ArrayList<Card>();
         for (int i = 0; i < 10; i++) {
             if (i % 1 == 0) {
                 //beginning of the array is the top of the deck
-                p1.getHand().add(deck.remove(0));
+                hand1.add(deck.remove(0));
             } else {
-                p2.getHand().add(deck.remove(0));
+                hand2.add(deck.remove(0));
             }
         }
-        //May need to sort differently, this sort works for Set Melds only
-        //Collections.sort(p1.getHand());
-        //Collections.sort(p2.getHand());
-        p1.receiveInitialHand(p1.getHand());
-        p2.receiveInitialHand(p2.getHand());
+
+        //Adding a card to the discard pile
+        discardPile.add(deck.get(0));
+        deck.remove(0);
+
+        //Both player hand are unsorted
+        p1.receiveInitialHand(hand1);
+        p2.receiveInitialHand(hand2);
     }
+
+/*
+    public static List<Card> getDiscardPile() {
+        return discardPile;
+    }
+*/
+
+    //-------------------------------------------------------------------------------
+    // Playing the first round
 
     //Assume that p1 is always the starting player
     public static void playFirstRound() {
-        //Run runMeld with p1's hand & get deadwood points
-        Meld playerRunMeld = Meld.buildRunMeld(p1.getHand());
-        int p1DwpRun = GameEngine.calculateDeadwoodPoints(playerRunMeld, p1.getHand());
-        //Run setMeld with p1's hand & get deadwood points
-        Meld playerSetMeld = Meld.buildSetMeld(p1.getHand());
-        int p1DwpSet = GameEngine.calculateDeadwoodPoints(playerSetMeld, p1.getHand());
-        //get the first card from the discard pile
         Card topCard = discardPile.get(0);
-        //compare 2 deadwood points from above & keep the meld with lower deadwood points
-        if (playerRunMeld != null || playerSetMeld != null) {
-            if (p1DwpRun > p1DwpSet) {
-                playerRunMeld = null;
-            } else {
-                playerSetMeld = null;
-            }
-            if (playerRunMeld.canAppendCard(topCard) || playerSetMeld.canAppendCard(topCard)) {
-
-            }
+        if (p1.knock()) {
+            //p1 reveals their cards
+            //p2 should be able to check if he can use his cards in p1's melds
+            //add the points to either p1 or p2
+            //start a new round
+            //this functionality should go in knock() itself
+            List<Meld> p1Melds = p1.getMelds();
+        } else if (p2.knock()) {
+            //p2 reveals their cards
+            //p1 should be able to check if he can use his cards in p2's melds
+            //add the points to either p1 or p2
+            //start a new round
+            //this functionality should go in knock() itself
+            List<Meld> p2Melds = p2.getMelds();
+        } else {
+            p1.willTakeTopDiscard(topCard);
         }
-        p1.willTakeTopDiscard(topCard);
-    }
 
+        //if p1 didn't choose from the discard pile, p2 automatically has the chance to choose from the discard pile
+        if (p1.willTakeTopDiscard(topCard) == false) {
+            p2.willTakeTopDiscard(topCard);
+            if (p2.willTakeTopDiscard(topCard)) {
+                p2.drawAndDiscard(topCard);
+            }
+        } else {
+            //if p1 chose from the discard pile, they have to handle their new card and discard another card from their hand
+            p1.drawAndDiscard(topCard);
+        }
+
+
+ /*       if(p1DeadwoodPoints == 0){
+            // Call p1.gin
+            //Calculate game settlement points and add/subtract from p1 & p1 current settlement points
+            //game over, start new game
+        }else if(p1DeadwoodPoints <= p1.getKnockLevel()){
+            // Call p1.knock
+            //P2 tries to fit his deadwood cards in melds of P1
+            //Calculate P2 deadwood points again
+            //Calculate game settlement points and add/subtract from p1 & p1 current settlement points
+        }else if(p2DeadwoodPoints == 0){
+            // Call p2.gin
+            //Calculate game settlement points and add/subtract from p1 & p1 current settlement points
+            //game over, start new game
+        }else if(p2DeadwoodPoints <= p2.getKnockLevel()){
+            // Call p2.knock
+            //P1 tries to fit his deadwood cards in melds of P2
+            //Calculate P1 deadwood points again
+            //Calculate game settlement points and add/subtract from p1 & p1 current settlement points
+            //game over, start new game
+        }else {*/
+            //p1 has option to pick the open card
+            //If p1 picks open card
+            // p1 add to exisitng melds or in deadwood card pile
+            //p1 will discard one card - this will mostly alter deadwood cards unless he throws the same picked card
+            //P1 calculates deadwood points
+            //else
+            //p2 has option to pick the open card
+            //If p2 picks open card
+            //p2 will discard one card - this will mostly alter deadwood cards unless he throws the same picked card
+            //P2 calculates deadwood points
+            //Again check p1 gin or knock and p2 gin or knock as above
+        }
+
+//        Following commented code to be removed
+//        boolean drewDiscarded = false;
+//        int p1DwpRun = GameEngine.calculateDeadwoodPoints(playerRunMeld, p1.getHand());
+//        //Run setMeld with p1's hand & get deadwood points
+//        Meld playerSetMeld = Meld.buildSetMeld(p1.getHand());
+//        int p1DwpSet = GameEngine.calculateDeadwoodPoints(playerSetMeld, p1.getHand());
+//        //get the first card from the discard pile
+//        Card topCard = discardPile.get(0);
+//        //compare 2 deadwood points from above & keep the meld with lower deadwood points
+//        if (playerRunMeld != null || playerSetMeld != null) {
+//            if (p1DwpRun > p1DwpSet) {
+//                playerRunMeld = null;
+//            } else {
+//                playerSetMeld = null;
+//            }
+//            if (playerRunMeld.canAppendCard(topCard) || playerSetMeld.canAppendCard(topCard)) {
+//
+//            }
+//        }
+//        p1.willTakeTopDiscard(topCard);
+
+    //-------------------------------------------------------------------------------
+    /*// Playing the Normal round
     public static void playNormalRound() {
 
     }
+    //-------------------------------------------------------------------------------
+    // Calculate deadwood points
+    public static int calculateDeadwoodPoints (List<Card> cardsNotInMeld, int deadwoodPoints) {
 
-    public static int calculateDeadwoodPoints(Meld playerMeld, List<Card> playerHand) {
-        if (playerMeld != null) {
-            for (Card cardObj : playerHand) {
-                if (!playerMeld.containsCard(cardObj)) {
-                    deadwoodPoints += cardObj.getPointValue();
-                }
-            }
-        } else {
-            for (Card cardObj : playerHand) {
-                deadwoodPoints += cardObj.getPointValue();
-            }
+        for (Card cardObj : cardsNotInMeld) {
+            deadwoodPoints += cardObj.getPointValue();
         }
-        return deadwoodPoints;
+        return deadwoodPoints;*/
+
+//    public static int calculateDeadwoodPoints(Meld playerMeld, List<Card> playerHand) {
+//        if (playerMeld != null) {
+//            for (Card cardObj : playerHand) {
+//                if (!playerMeld.containsCard(cardObj)) {
+//                    deadwoodPoints += cardObj.getPointValue();
+//                }
+//            }
+//        } else {
+//            for (Card cardObj : playerHand) {
+//                deadwoodPoints += cardObj.getPointValue();
+//            }
+//        }
+
     }
-
-
-
-
-}
+//--------------------------------------------------------------------------------
+//}
--- src/com/example/Shivani.java
+++ src/com/example/Shivani.java
@@ -5,15 +5,20 @@ import com.example.Meld;
 import com.example.PlayerStrategy;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Shivani implements PlayerStrategy{
 
+    //The player's hand of cards
     private List<Card> hand = new ArrayList<Card>();
+    private static List<Card> cardsNotInMeld = new ArrayList<Card>();
+    private static List<Meld> totalMelds = new ArrayList<Meld>();
+    private static int deadwoodPoints;
+    private static int knockLevel = 7;
 
-    public List<Card> getHand() {
-        return hand;
-    }
+    private static boolean isKnocking = false;
 
     /**
      * Called by the game engine for each player at the beginning of each round to receive and
@@ -22,7 +27,11 @@ public class Shivani implements PlayerStrategy{
      * @param hand The initial hand dealt to the player
      */
     public void receiveInitialHand(List<Card> hand){
-        this.hand = hand;
+        cardsNotInMeld = hand;
+        Shivani.createRunMeld();
+        Shivani.createSetMeld();
+        deadwoodPoints = Shivani.calculateDeadwoodPoints();
+        knock();
     }
 
     /**
@@ -33,26 +42,191 @@ public class Shivani implements PlayerStrategy{
      * @return whether the user takes the card on the discard pile
      */
     public boolean willTakeTopDiscard(Card card){
-        return false;
+        boolean tookDiscardCard = false;
+        for (Meld meldObj : totalMelds) {
+            if (meldObj.canAppendCard(card)) {
+                tookDiscardCard = true;
+                meldObj.appendCard(card);
+                break;
+            }
+        }
+        return tookDiscardCard;
     }
 
+    /**
+     * Called by the game engine to prompt the player to take their turn given a
+     * dealt card (and returning their card they've chosen to discard).
+     *
+     * @param drawnCard The card the player was dealt
+     * @return The card the player has chosen to discard
+     */
     public Card drawAndDiscard(Card drawnCard){
         return null;
     }
 
+    /**
+     * Called by the game engine to prompt the player if whether they would like to
+     * knock.
+     *
+     * @return True if the player has decided to knock
+     */
     public boolean knock(){
-        return false;
+        if (deadwoodPoints <= knockLevel) {
+            isKnocking = true;
+        }
+        return isKnocking;
     }
 
-    public void opponentEndTurnFeedback(boolean drewDiscard, Card previousDiscardTop, Card opponentDiscarded){}
+    /**
+     * Called by the game engine when the opponent has finished their turn to provide the player
+     * information on what the opponent just did in their turn.
+     *
+     * @param drewDiscard Whether the opponent took from the discard
+     * @param previousDiscardTop What the opponent could have drawn from the discard if they chose to
+     * @param opponentDiscarded The card that the opponent discarded
+     */
+    public void opponentEndTurnFeedback(boolean drewDiscard, Card previousDiscardTop, Card opponentDiscarded){
 
-    public void opponentEndRoundFeedback(List<Card> opponentHand, List<Meld> opponentMelds){}
+    }
 
+    /**
+     * Called by the game engine when the round has ended to provide this player strategy
+     * information about their opponent's hand and selection of Melds at the end of the round.
+     *
+     * @param opponentHand The opponent's hand at the end of the round
+     * @param opponentMelds The opponent's Melds at the end of the round
+     */
+    public void opponentEndRoundFeedback(List<Card> opponentHand, List<Meld> opponentMelds){
+
+    }
+
+    /**
+     * Called by the game engine to allow access the player's current list of Melds.
+     *
+     * @return The player's list of melds.
+     */
     public List<Meld> getMelds(){
-        return null;
+        return totalMelds;
     }
 
-    public void reset(){}
+    /**
+     * Called by the game engine to allow this player strategy to reset its internal state before
+     * competing it against a new opponent.
+     */
+    public void reset(){
+
+    }
+
+    //-------------------------------------------------------------------------------
+    // 0 to 3 melds are possible, combination of run meld and set meld
+    // First try to create run melds for each suit
+    //Then try to create suit melds of remaining cards
+    //Then remaining cards are deadwood card
+    //-------------------------------------------------------------------------------
+
+    //Run Meld creation
+    public static void createRunMeld() {
+
+        //RunSort: Sort cardsNotInMeld (initially full hand) based on suit and then rank
+        List<Card> p1PotentialRunMeld = new ArrayList<Card>(cardsNotInMeld);
+        //Shuffling the cards cardsNotInMeld to test if RunSort worked - Comment/remove after test
+        Collections.shuffle(p1PotentialRunMeld);
+
+        Collections.sort(p1PotentialRunMeld, new Comparator<Card>() {
+            @Override
+            public int compare(Card o1, Card o2) {
+                int suitDifference = o1.getSuit().ordinal() - o2.getSuit().ordinal();
+                if (suitDifference == 0) {
+                    return o1.getRankValue() - o2.getRankValue();
+                }
+                return suitDifference;
+            }
+        });
+
+        //Iterate through sorted cardsNotInMeld (initially full hand) = p1PotentialRunMeld
+//        int startingIndex = 0;
+//        int endingIndex = p1PotentialRunMeld.size() - 1;
+//        while (startingIndex < endingIndex) {
+//            //Check if all cardsNotInMeld are of same rank, if yes do what?
+//            if (p1PotentialRunMeld.get(startingIndex).getRank() == p1PotentialRunMeld.get(endingIndex).getRank()) {
+//
+//            }
+//            List<Card> p1PotentialRunMeld2 = new ArrayList<Card>(p1PotentialRunMeld);
+//            //if (p1PotentialRunMeld.get(0).getRank()) {
+//                //special case- check to see if the there is a run of 10 cards, check 10th element to see if it is
+//                //equal to first element + 9
+//            //}
+//        }
+        //List<Card> p1PotentialRunMeld2 = new ArrayList<Card>();
+        //p1PotentialRunMeld2.add(p1PotentialRunMeld.get(0));
+        //p1PotentialRunMeld2.add(p1PotentialRunMeld.get(1));
+        //RunMeld p1RunMeld = Meld.buildRunMeld(p1PotentialRunMeld);
+        System.out.println("");
+    }
+
+
+    //Set Meld creation
+    public static void createSetMeld() {
+        //Sort remaining cards based on rank to create Set Meld
+        List<Card> p1PotentialSetMeld = new ArrayList(cardsNotInMeld);
+        //Shuffling the cards cardsNotInMeld to test if SetSort worked - Comment/remove after test
+        Collections.shuffle(p1PotentialSetMeld);
+
+        Collections.sort(p1PotentialSetMeld, new Comparator<Card>() {
+            @Override
+            public int compare(Card o1, Card o2) {
+                return o1.getRankValue() - o2.getRankValue();
+            }
+        });
+
+        //Iterate through the sorted cards
+        //iterator i - if the 4th element is the same, you can assume that the the cards in the middle
+        // are the same because they are sorted
+        int i = 0;
+        while (i < p1PotentialSetMeld.size()) {
+            boolean canCheck4 = (i + 3) < p1PotentialSetMeld.size();
+            boolean canCheck3 = (i + 2) < p1PotentialSetMeld.size();
+            if (canCheck4 && p1PotentialSetMeld.get(i).getRank().equals(p1PotentialSetMeld.get(i + 3).getRank())) {
+                List<Card> p1PotentialSetMeld2 = new ArrayList<Card>();
+                p1PotentialSetMeld2.add(p1PotentialSetMeld.get(i));
+                p1PotentialSetMeld2.add(p1PotentialSetMeld.get(i + 1));
+                p1PotentialSetMeld2.add(p1PotentialSetMeld.get(i + 2));
+                p1PotentialSetMeld2.add(p1PotentialSetMeld.get(i + 3));
+                cardsNotInMeld.remove(p1PotentialSetMeld.get(i));
+                cardsNotInMeld.remove(p1PotentialSetMeld.get(i + 1));
+                cardsNotInMeld.remove(p1PotentialSetMeld.get(i + 2));
+                cardsNotInMeld.remove(p1PotentialSetMeld.get(i + 3));
+                SetMeld p1SetMeld = Meld.buildSetMeld(p1PotentialSetMeld2);
+                totalMelds.add(p1SetMeld);
+                i += 4;
+            } else if(canCheck3 && p1PotentialSetMeld.get(i).getRank().equals(p1PotentialSetMeld.get(i + 2).getRank()))
+            {
+                List<Card> p1PotentialSetMeld3 = new ArrayList<Card>();
+                p1PotentialSetMeld3.add(p1PotentialSetMeld.get(i));
+                p1PotentialSetMeld3.add(p1PotentialSetMeld.get(i + 1));
+                p1PotentialSetMeld3.add(p1PotentialSetMeld.get(i + 2));
+                cardsNotInMeld.remove(p1PotentialSetMeld.get(i));
+                cardsNotInMeld.remove(p1PotentialSetMeld.get(i + 1));
+                cardsNotInMeld.remove(p1PotentialSetMeld.get(i + 2));
+                SetMeld p1SetMeld2 = Meld.buildSetMeld(p1PotentialSetMeld3);
+                totalMelds.add(p1SetMeld2);
+                i += 3;
+            } else {
+                i++;
+            }
+        }
+        //-------------------------------------------------------------------------------
+    }
+
+    // Calculate deadwood points
+    public static int calculateDeadwoodPoints() {
+        deadwoodPoints = 0;
+        for (Card cardObj : cardsNotInMeld) {
+            deadwoodPoints += cardObj.getPointValue();
+        }
+        return deadwoodPoints;
+
+    }
 
 
 }
